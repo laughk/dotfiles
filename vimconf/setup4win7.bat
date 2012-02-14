c79a3e6402eda934b5f35423e3cfbaf336ee3818
Kei Iwasaki <laughk.android@gmail.com>
2012-02-14 10:45:40 +0900

    Windwsの設定でシンボリックリンクを使うと設定変更時にリンクが切れるためハードリンクで設定するように変更。

--- vimconf/setup4win7.bat
+++ vimconf/setup4win7.bat
@@ -1,13 +1,18 @@
-:// ホームディレクトリにシンボリックリンクを張る
-mklink %homepath%\_vimrc %cd%\vimrc
-mklink %homepath%\_gvimrc %cd%\gvimrc
+: =====================================
+: ホームディレクトリにハードリンクを張る（シンボリックリンクだと編集でリンクが切れるため）
+: =====================================
+mklink /H %homepath%\_vimrc %cd%\vimrc
+mklink /H %homepath%\_gvimrc %cd%\gvimrc
 mklink /D %homepath%\vimfiles %cd%\vim
 
-:// bundleディレクトリを作成
-:// ※ submodule の挙動がおかしくなるため、gitでここは管理しない
+: ====================================
+: bundleディレクトリを作成
+: ※ submodule の挙動がおかしくなるため、gitでここは管理しない
+: ====================================
 mkdir %cd%\vim\bundle
-:// Vundle初回導入
+
+: Vundle初回導入  ----------------------------------------------
 git clone https://github.com/gmarik/vundle.git %cd%\vim\bundle\vundle
 
-:// submodule に追加しているプラグインをインストールする
+: submodule に追加しているプラグインをインストールする --------
 git submoduele update --init
--- vimconf/setup4winXP.bat
+++ vimconf/setup4winXP.bat
@@ -1,13 +1,19 @@
-:// ホームディレクトリにシンボリックリンクを張る
+: ===============================================
+: ホームディレクトリにハードリンクを張る
+:  ※ linkd コマンドは要 windows server 2003 resource kit tools
+: ===============================================
 fsutil hardlink create "%USERPROFILE%\_vimrc" "%CD%\vimrc"
 fsutil hardlink create "%USERPROFILE%\_gvimrc" "%CD%\gvimrc"
 linkd "%USERPROFILE%\vimfiles" "%CD%\vim"
 
-:// bundleディレクトリを作成
-:// ※ submodule の挙動がおかしくなるため、gitでここは管理しない
+: ===============================================
+: bundleディレクトリを作成
+: ※ submodule の挙動がおかしくなるため、gitでここは管理しない
+: ===============================================
 mkdir "%CD%\vim\bundle"
-:// Vundle初回導入
+
+: Vundle初回導入 -------------------------------------------------
 git clone https://github.com/gmarik/vundle.git vim\bundle\vundle
 
-:// submodule に追加しているプラグインをインストールする
+: submodule に追加しているプラグインをインストールする -----------
 git submoduele update --init
