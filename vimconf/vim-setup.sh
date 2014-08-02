84e230278e475b3cdfb9a187e9d0e8f19c82e64a
Kei Iwasaki <laughk.android@gmail.com>
2014-08-02 14:50:31 +0900

    バグだらけだったvim設定スクリプトを修正

--- vimconf/vim-setup.sh
+++ vimconf/vim-setup.sh
@@ -2,100 +2,136 @@
 # -----------------------------------------------------
 # Default Config
 # -----------------------------------------------------
+SCRIPT_NAME=${0##*/}
 SCRIPT_DIR="$(cd $(dirname $0) && pwd )"
 MODE="help"
-MODE_GUI="on"
+MODE_GUI="off"
 VIMVERSION=$( vim --version | awk 'NR ==1 {print $5}' )
 
 # -----------------------------------------------------
 # Functions
 # -----------------------------------------------------
-function make_link_cli(){
-  if [ ! -e ${HOME}/.vim ]   ; then
-    ln -s ${SCRIPT_DIR}/vim ${HOME}/.vim \
-      && echo "vim conf dir Set OK!"
+function make_link(){
+
+  local _gui=$1
+  local _targets="vim"
+
+  if [[ ${VIMVERSION} =~ 7.[0-3] ]] ; then
+
+    _targets="$_targets vim/vimrc"
+
+    if [[ $_gui = on ]] ; then
+      _target="$_targets vim/gvimrc"
+    fi
+
   fi
 
-  if [ ! -e ${HOME}/.vimrc  -a $( echo ${VIMVERSION} < 7.4 | bc ) -eq 1 ] ; then
-    ln -s ${SCRIPT_DIR}/vimrc ${HOME}/.vimrc \
-      && echo "vimrc Set OK!"
-  fi
+  for t in ${_targets} ; do
+    ln -fs ${SCRIPT_DIR}/$t ${HOME}/.${t#vim/} && echo "${HOME}/.${t#vim/} Set OK!"
+  done
+
 }
 
-function make_link_gui(){
-  if [ ! -e ${HOME}/.gvimrc -a $( echo ${VIMVERSION} < 7.4 | bc ) -eq 1 ] ; then
-    ln -s ${SCRIPT_DIR}/gvimrc ${HOME}/.gvimrc \
-      && echo "gvimrc Set OK!"
+
+function del_link() {
+
+  local _gui=$1
+  local _targets=".vim"
+
+  if [[ ${VIMVERSION} =~ 7.[0-3] ]] ; then
+
+    _targets="$_targets .vimrc"
+
+    if [[ $_gui = on ]] ; then
+      _target="$_targets .gvimrc"
+    fi
+
   fi
+
+  for t in ${_targets} ; do
+    if [[ -e ${HOME}/$t ]] ; then
+      rm -f ${HOME}/$t && echo "Delete link ${HOME}/$t "
+    else
+      echo "Not exist ${HOME}/$t "
+    fi
+  done
+
 }
 
-function del_link_cli(){
-  if [ -e ${HOME}/.vimrc ] ; then
-     rm -f ~/.vimrc && echo "Delete link ${HOME}/.vimrc "
-  fi
-  if [ -e ${HOME}/.vim ]   ; then
-     rm -f ~/.vim   && echo "Delete link ${HOME}/.vim "
-  fi
-}
-
-function del_link_gui(){
-  if [ -e ${HOME}/.gvimrc ] ; then
-     rm -f ~/.vimrc && echo "Delete link ${HOME}/.gvimrc "
-  fi
-}
 
 function clone_neobundle(){
-  if [ -d ${SCRIPT_DIR}/vim/bundle ] ; then
-    echo "Already exist 'bundle' !!"
-  else
+
+  if [[ ! -d ${SCRIPT_DIR}/vim/bundle ]] ; then
     mkdir ${SCRIPT_DIR}/vim/bundle \
-    && echo "Neobundle install Dir MADE OK!"
+    && echo "Make Neobundle install Dir"
   fi
 
   git clone git://github.com/Shougo/neobundle.vim.git ${SCRIPT_DIR}/vim/bundle/neobundle.vim
+
 }
+
 
 function del_plugins(){
   rm -rf ${SCRIPT_DIR}/vim/bundle
 }
 
+
 function usage_msg(){
-  echo "Usage: $(basename $0) [-c] {setup|unlink|pdelete|delete|help}"
+  echo "Usage: $SCRIPT_NAME [-c] [-m {setup|unlink|pdelete|delete}] [-h]"
 }
+
 
 function help_msg(){
 cat << _HELP_MSG_
 
 ***OPTIONS***
--c: cli only mode on / Default 'off'
+-g: setup for gvim / Default 'off'
+-m: mode select
+-h: This help Message
 
 ***MODE SELECT***
   setup     : setup vim confing
-          (make symlink and clone Neobundle)
+              (make symlink and clone Neobundle)
   unlink    : delete symlink (ex. ~/.vimrc, ~/.vim )
-          not delete Neobundle
-  pdelete     : delete all installed vim plugins
-          not delete symlink
+              not delete Neobundle
+  pdelete   : delete all installed vim plugins
+              not delete symlink
   delete    : delete all of vim config
-          (delete symlink and Neobundle and all plugin)
-
-  help(default) : This help Message
+              (delete symlink and Neobundle and all plugin)
 
 _HELP_MSG_
+
 }
 
 
 # -----------------------------------------------------
 # Option,Param Check
 # -----------------------------------------------------
-while getopts "m:ch" GETOPTS ; do
-  case ${GETOPTS} in
-    m) MODE="${OPTARG}"
-       ;;
-    c) MODE_GUI="off"
-       ;;
-    h|*) MODE="help"
-       ;;
+if [[ -z "$@" ]] ; then
+  usage_msg
+  help_msg
+  exit 1
+fi
+
+for OPT in $@ ; do
+  case "$OPT" in
+    '-h'|'--help')
+        usage_msg
+        help_msg
+        exit 1
+        ;;
+    '-g'|'--enable-gui')
+        MODE_GUI="on"
+        shift
+        ;;
+    '-m'|'--mode')
+        if [[ -z "$2" ]] || [[ "$2" =~ ^-+ ]] ; then
+          echo "$SCRIPT_NAME: option require an argument -- $1" 1>&2
+          exit 1
+        fi
+        MODE="$2"
+        shift 2
+        ;;
   esac
 done
 
@@ -104,30 +140,25 @@ done
 # -----------------------------------------------------
 case $MODE in
   'setup')
-       make_link_cli
-       [ "${MODE_GUI}" = "on" ] && make_link_gui
+       make_link ${MODE_GUI}
        clone_neobundle
        exit 0
        ;;
-
   'unlink')
-       del_link_cli
-       [ "${MODE_GUI}" = "on" ] && del_link_gui
+       del_link ${MODE_GUI}
        exit 0
        ;;
-
   'pdelete')
        del_plugins
        exit 0
        ;;
 
-   'delete')
-       del_link_cli
-       [ "${MODE_GUI}" = "on" ] && del_link_gui
+  'delete')
+       del_link ${MODE_GUI}
        del_plugins
        exit 0
        ;;
-   'help'|'h')
+  *)
        usage_msg && help_msg
        exit 1
        ;;
