8754bf2bfdd9dc626864dfe776af64de56184788
Kei Iwasaki <me@laughk.org>
2016-02-27 11:37:41 +0900

    virtualenv/venv activate の alias を追加

--- zsh/conf.d/alias.zsh
+++ zsh/conf.d/alias.zsh
@@ -1,15 +1,18 @@
 #! zsh
 
-# Alias
-# ----------------------------------------
+
 alias tmux="tmux -2"
+alias py_active="source venv/bin/activate"
+
 if [[ $(uname) = Darwin ]] ; then
+  # Macでだけ必要なもの
   alias find="gfind"
   alias ls='ls -GF'
   alias vim="/Applications/MacVim.app/Contents/MacOS/Vim"
   alias mvim="/Applications/MacVim.app/Contents/MacOS/mvim"
   alias gvim=mvim
 else
+  # Linux(Mac以外, Gnu系)で必要なもの
   alias ls='ls --color=auto -F'
 fi
 
