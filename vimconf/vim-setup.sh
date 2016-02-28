bbb88524193683c93b5d3ab7cbe01e5ad42c185b
Kei Iwasaki <me@laughk.org>
2016-02-28 14:38:31 +0900

    すでにNeoBundleは使ってなかった

--- vimconf/vim-setup.sh
+++ vimconf/vim-setup.sh
@@ -5,11 +5,6 @@
 # setup
 function setup() {
   ln -svf ~/.dotfiles/vim ~/.vim
-  [[ ! -d ~/.vim/bundle/neobundle.vim ]] && {
-    git clone \
-      https://github.com/Shougo/neobundle.vim \
-      ~/.vim/bundle/neobundle.vim
-  }
 }
 
 # clean
