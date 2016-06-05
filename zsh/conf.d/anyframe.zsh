851b04ec0912ca7046705afca521961e9c005582
Kei Iwasaki <me@laughk.org>
2016-06-05 16:20:48 +0900

    anyframe の設定を更新

--- zsh/conf.d/anyframe.zsh
+++ zsh/conf.d/anyframe.zsh
@@ -1,21 +1,27 @@
 zstyle ":anyframe:selector:" use fzf
 zstyle ":anyframe:selector:fzf:" command 'fzf --extended'
 
+## 移動したことのあるディレクトリへ移動
+autoload -Uz chpwd_recent_dirs cdr add-zsh-hook
+add-zsh-hook chpwd chpwd_recent_dirs
+bindkey '^xd' anyframe-widget-cdr
 
-bindkey '^xb' anyframe-widget-cdr
-bindkey '^x^b' anyframe-widget-checkout-git-branch
-
+## history 操作
+### そのまま実行する
 bindkey '^xr' anyframe-widget-execute-history
 bindkey '^x^r' anyframe-widget-execute-history
-
+### 表示のみ
 bindkey '^xi' anyframe-widget-put-history
 bindkey '^x^i' anyframe-widget-put-history
 
-bindkey '^xg' anyframe-widget-cd-ghq-repository
-bindkey '^x^g' anyframe-widget-cd-ghq-repository
-
+### プロセスをkillする
 bindkey '^xk' anyframe-widget-kill
 bindkey '^x^k' anyframe-widget-kill
 
-bindkey '^xe' anyframe-widget-insert-git-branch
-bindkey '^x^e' anyframe-widget-insert-git-branch
+## ghq管理リポジトリ
+bindkey '^xg' anyframe-widget-cd-ghq-repository
+bindkey '^x^g' anyframe-widget-cd-ghq-repository
+
+## git ブランチ操作
+bindkey '^xb' anyframe-widget-insert-git-branch
+bindkey '^x^b' anyframe-widget-checkout-git-branch
