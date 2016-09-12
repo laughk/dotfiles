9895959a87eb997a3a7d3cc2469fcf78a575354c
Kei Iwasaki <iwasaki@pepabo.com>
2016-09-12 20:29:18 +0900

    :+1: auto_pushd が有効になってなかったのでちょこちょこ修正
    cd -<tab><tab>
    
    で最近移動したディレクトリ一覧出せるやつ。

--- zsh/conf.d/base.zsh
+++ zsh/conf.d/base.zsh
@@ -5,8 +5,21 @@ export HIST_STAMPS="yyyy-mm-dd"
 export SAVEHIST=10000000
 export HISTSIZE=$SAVEHIST
 
+# history を共有
+setopt share_history
+
+# 補完を強化
 autoload -U compinit
 compinit -u
+
+# 自動的にpushd
+setopt auto_pushd
+
+# 補完候補をカーソル選択できるように
+zstyle ':completion:*:default' menu select=1
+
+## 補完候補の色づけ
+zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS}
 
 PATH="${HOME}/local/bin:${PATH}"
 export PATH
