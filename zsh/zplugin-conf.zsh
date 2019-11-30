cd9f5143fa33a4f20bc02688ab2d9591facc34f9
Kei IWASAKI <me@laughk.org>
2019-11-30 15:58:52 +0900

    feat: goodbye zplug and hello zplugin

---
+++ zsh/zplugin-conf.zsh
@@ -0,0 +1,36 @@
+#! zsh
+
+# Plugin
+# ----------------------------------------
+zplugin load mollifier/anyframe
+
+zplugin ice wait"3"
+zplugin load zsh-users/zsh-syntax-highlighting
+
+# CLI
+# ----------------------------------------
+zplugin ice as"program" from"gh-r" bpick"*linux64" mv"jq-* -> jq"
+zplugin load stedolan/jq
+
+zplugin ice as"program" from"gh-r" bpick"*linux_amd64*" pick"*/ghq"
+zplugin load motemen/ghq
+
+zplugin ice as"program" from"gh-r" bpick"*linux_amd64*"
+zplugin load junegunn/fzf-bin 
+
+zplugin ice as"program" from"gh-r" bpick"*linux_amd64*" pick"*/pt"
+zplugin load monochromegane/the_platinum_searcher
+
+zplugin ice as"program" from"github" pick"bin/fzf-tmux"
+zplugin load junegunn/fzf 
+
+# Theme
+# ----------------------------------------
+# for using theme by oh-my-zsh {{{
+zplugin snippet OMZ::lib/git.zsh
+zplugin snippet OMZ::plugins/git/git.plugin.zsh
+zplugin cdclear -q 
+setopt promptsubst
+# }}}
+
+zplugin snippet https://raw.githubusercontent.com/caiogondim/bullet-train.zsh/master/bullet-train.zsh-theme
