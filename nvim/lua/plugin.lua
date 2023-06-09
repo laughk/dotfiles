8700ee3c7130d07d5ba25ff5c607702e7da1194b
Kei IWASAKI <me@laughk.org>
2023-06-09 12:31:49 +0900

    feat: fzf-lua は Windows だと使えないので telescope.nvim に移行

--- nvim/lua/plugin.lua
+++ nvim/lua/plugin.lua
@@ -313,8 +313,23 @@ local plugins = {
     end,
   },
   {
-    'ibhagwan/fzf-lua',
-    dependencies = { 'kyazdani42/nvim-web-devicons' }
+    'nvim-telescope/telescope.nvim',
+    tag = '0.1.1',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function()
+      local builtin = require('telescope.builtin')
+      vim.keymap.set('n', 'ff', builtin.find_files, {})
+      vim.keymap.set('n', 'fg', builtin.live_grep, {})
+      vim.keymap.set('n', 'fb', builtin.buffers, {})
+      vim.keymap.set('n', 'fh', builtin.help_tags, {})
+    end,
+  },
+  {
+    'zioroboco/nu-ls.nvim',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+  },
+  {
+    'google/vim-jsonnet',
   }
 }
 
